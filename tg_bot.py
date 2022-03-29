d8875d408de0c8db329c0b037015776665d6d2b5
949027 <82878668+949027@users.noreply.github.com>
2022-03-29 10:24:34 +0700

    Removed hardwired value. Renamed handler

--- tg_bot.py
+++ tg_bot.py
@@ -8,16 +8,19 @@ from environs import Env
 
 from dialogflow import detect_intent_texts
 
+env = Env()
+env.read_env()
+
 
 def start(update: Update, context: CallbackContext):
     context.bot.send_message(
         chat_id=update.effective_chat.id,
-        text="I'm a bot, please talk to me!"
+        text="Привет, я бот! Поговори со мной!"
     )
 
 
 def to_answer(update: Update, context: CallbackContext):
-    project_id = 'high-extension-344009'
+    project_id = env('DIALOGFLOW_PROJECT_ID')
     chat_id = update.effective_chat.id
     user_text = update.message.text
     response = detect_intent_texts(project_id, chat_id, [user_text])
@@ -29,9 +32,6 @@ def to_answer(update: Update, context: CallbackContext):
 
 
 def main():
-    env = Env()
-    env.read_env()
-
     logging.basicConfig(
         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
         level=logging.INFO
@@ -40,13 +40,13 @@ def main():
     updater = Updater(token=env('TELEGRAM_TOKEN'), use_context=True)
     dispatcher = updater.dispatcher
 
-    echo_handler = MessageHandler(
+    answer_handler = MessageHandler(
         Filters.text & (~Filters.command),
         to_answer,
     )
     start_handler = CommandHandler('start', start)
     dispatcher.add_handler(start_handler)
-    dispatcher.add_handler(echo_handler)
+    dispatcher.add_handler(answer_handler)
 
     updater.start_polling()
 
